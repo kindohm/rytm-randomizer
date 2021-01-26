757f55752145630ca1d08d7412c2079d7e45eabe
Mike Hodnick <mike@kindohm.com>
2021-01-26 12:14:42 -0600

    update footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="container">
       <footer>
         <small>
-          Developed by <a href="//kindohm.com">kindohm</a>. Inspired by the{' '}
-          <a href="//algorithmic-instruments.com/dsi-tempest-randomizer">
+          Developed by <a href="http://kindohm.com">kindohm</a>. Inspired by the{' '}
+          <a href="http://algorithmic-instruments.com/dsi-tempest-randomizer">
             DSI Tempest Randomizer
           </a>
         </small>
